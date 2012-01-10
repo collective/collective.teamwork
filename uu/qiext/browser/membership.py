6d5988e5cdf7e3346ff660fba32211ea116cd637
Sean Upton <sdupton@gmail.com>
2012-01-10 00:42:47 -0700

    skeleton class, template for membership management view for workspace types (project, team, etc).

---
+++ uu/qiext/browser/membership.py
@@ -0,0 +1,20 @@
+
+
+
+class WorkspaceMembership(object):
+    """
+    Workspace membership view, provides a front-end around
+    backend adapters for workspace in uu.qiext.user modules.
+    """
+    
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+   
+    def update(self, *args, **kwargs):
+        pass #TODO implement as needed
+     
+    def __call__(self, *args, **kwargs):
+        self.update(*args, **kwargs)
+        self.index(*args, **kwargs)  # provided by Five magic
+
