a979cd19917d9793399e4be5f409612a51cd7aba
Sean Upton <sdupton@gmail.com>
2012-02-09 07:43:36 -0700

    removal handler tests

--- uu/qiext/tests/test_handlers.py
+++ uu/qiext/tests/test_handlers.py
@@ -88,4 +88,40 @@ class HandlerTest(unittest.TestCase):
             assert g not in allgroups_postrename    # old names
         for g in ['-'.join((proj_id1+'a', team_id1+'a', s)) for s in suffixes]:
             assert g in allgroups_postrename        # new names
+    
+    def test_remove(self):
+        """test removal of project and team, make sure no orphan groups"""
+        adapter = CreateContentFixtures(self, self.layer)
+        suffixes = WORKSPACE_GROUPS.keys()
+        proj_id1 = 'proj_handler_test_remove'
+        self.assertNotIn(proj_id1, self.portal.contentIds())
+        allgroups_before = self.groups_plugin.listGroupIds()
+        proj = adapter.add_project(proj_id1)
+        self.assertIn(proj_id1, self.portal.contentIds())
+        allgroups_after = self.groups_plugin.listGroupIds()
+        self.assertEquals(len(allgroups_after) - len(allgroups_before), 4)
+        for g in ['%s-%s' % (proj_id1, suffix) for suffix in suffixes]:
+            assert g in allgroups_after
+        ## now create a team workspace inside the project, similarly:
+        team_id1 = 'team1'
+        allgroups_before = self.groups_plugin.listGroupIds()
+        for g in ['-'.join((proj_id1, team_id1, s)) for s in suffixes]:
+            assert g not in allgroups_before
+        team = adapter.add_team_to(proj, team_id1)
+        allgroups_after = self.groups_plugin.listGroupIds()
+        ## necessary/sufficient: all expected groups (and only these):
+        self.assertEquals(len(allgroups_after) - len(allgroups_before), 4)
+        for g in ['-'.join((proj_id1, team_id1, s)) for s in suffixes]:
+            assert g in allgroups_after
+        ## now remove from inside-out, starting with team
+        proj.manage_delObjects([team_id1,])
+        allgroups_postdel = self.groups_plugin.listGroupIds()
+        self.assertEquals(len(allgroups_after) - len(allgroups_postdel), 4)
+        for g in ['-'.join((proj_id1, team_id1, s)) for s in suffixes]:
+            assert g not in allgroups_after
+        self.portal.manage_delObjects([proj_id1,])
+        allgroups_donedel = self.groups_plugin.listGroupIds()
+        self.assertEquals(len(allgroups_postdel) - len(allgroups_donedel), 4)
+        for g in ['-'.join((proj_id1, team_id1, s)) for s in suffixes]:
+            assert g not in allgroups_donedel
 
