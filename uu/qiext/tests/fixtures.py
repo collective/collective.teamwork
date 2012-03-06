fafb8d219a4aa92fb4b40c64048f2ac9d879a1ec
Sean Upton <sdupton@gmail.com>
2012-03-06 15:49:11 -0700

    add project welcome page and test member to project workspace in text fixtures

--- uu/qiext/tests/fixtures.py
+++ uu/qiext/tests/fixtures.py
@@ -1,10 +1,13 @@
 # content fixtures shared across tests -- multi-adapter component 
 # adapts test-suite and test layer
 
+from plone.app.layout.navigation.defaultpage import isDefaultPage
+
 from Products.qi.extranet.types.interfaces import IProject, ITeam
 from Products.qi.extranet.types.project import Project
 from Products.qi.extranet.types.team import Team
 
+from uu.qiext.user.interfaces import ISiteMembers, IWorkspaceRoster
 
 class CreateContentFixtures(object):
     """
@@ -12,6 +15,9 @@ class CreateContentFixtures(object):
     we only want it once... whichever test suite calls this first will
     create the content, others will rely on that as given.
     """
+    
+    TEST_MEMBER = 'me@example.com'
+    
     def __init__(self, context, layer):
         self.context = context #suite
         self.layer = layer
@@ -32,16 +38,24 @@ class CreateContentFixtures(object):
         self.context.assertTrue(iface.providedBy(o))
         o.reindexObject()
         return o # return constructed content for use in additional testing
-
+    
     def add_project(self, id, title=None):
-        return self.add_check('qiproject', id, IProject, Project)
-
+        project = self.add_check('qiproject', id, IProject, Project)
+        members = ISiteMembers(self.portal)
+        members.register(self.TEST_MEMBER, send=False)
+        assert self.TEST_MEMBER in members
+        roster = IWorkspaceRoster(project)
+        roster.add(self.TEST_MEMBER)
+        assert self.TEST_MEMBER in roster
+        return project
+    
     def add_team_to(self, parent, id, title=None):
         return self.add_check('qiteam', id, ITeam, Team, parent=parent)
-
+    
     def create(self):
         """
         project1/
+        project1/welcome        (page set as default page for project)
         project1/team1/
         project1/team1/stuff    (folder)
         project1/team2
@@ -52,6 +66,10 @@ class CreateContentFixtures(object):
             return # run once, already run
         portal = self.portal
         project = self.add_project('project1')
+        project.invokeFactory('Document', 'welcome', title='Welcome')
+        welcome = project['welcome']
+        project.default_page = 'welcome'
+        assert isDefaultPage(container=project, obj=welcome)
         team1 = self.add_team_to(project, 'team1')
         team1.invokeFactory('Folder', 'stuff', title='Normal folder')
         team2 = self.add_team_to(project, 'team2')
