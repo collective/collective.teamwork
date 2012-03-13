ea5b44b497d99c28cda5101e3caa85109e31b38d
Sean Upton <sdupton@gmail.com>
2012-03-13 15:22:56 -0600

    created resource directory and split JS and CSS for membership.pt out of template into resource file, adjusted CSS urls relative to stylesheet, not document for two background images; membership.pt now uses portal root url for all images and for stylesheet and javascript refs.

---
+++ uu/qiext/browser/resources/membership.js
@@ -0,0 +1,68 @@
+/* JavaScript for membership management page for uu.qiext */
+
+jQuery(document).ready(function() {
+    /* overlays */
+    jQuery('a.add-user-entry').prepOverlay({
+         subtype: 'ajax',
+         filter: 'div.page_section.existing_users',
+         formselector: 'div.existing_users form.user_search',
+        });
+    jQuery('a.register-user-entry').prepOverlay({
+         subtype: 'ajax',
+         filter: 'div.page_section.register_user',
+        });
+    jQuery('td.userinfo a.userinfo').prepOverlay({
+         subtype: 'ajax',
+         filter: '#content',
+        });
+    jQuery('td.gridcell a.purge').prepOverlay({
+         subtype: 'ajax',
+         filter: '#content',
+         formselector: '#content form.user-purge-form',
+         closeselector: '#content form.user-purge-form input.cancelbutton',
+         noform: 'reload',
+         config: {
+            onClose: function(e) {
+                var input = jQuery('#content form.user-purge-form input.cancelbutton');
+                if (input.val() == 'Ok') {
+                    /* on success (not cancel): reload after either GET, POST, idea via http://goo.gl/Lrc4j */
+                    window.location.href = window.location.pathname + window.location.search;
+                }
+            }
+         }
+        });
+    jQuery('a.password-reset').prepOverlay({
+         subtype: 'ajax',
+         filter: '#content form',
+         formselector: 'form[name="mail_password"]',
+         noform: function(e) { alert('User will be sent password reset email shortly.'); return 'close'; }
+        });
+    jQuery('a.view-roster').prepOverlay({
+         subtype: 'ajax',
+         filter: '#content',
+         config: {
+            onLoad: function(e) {
+                jQuery('div.pb-ajax a.userinfo').prepOverlay({
+                    subtype: 'ajax', filter: '#content',
+                    config: { onBeforeLoad: function(e) { jQuery('div.overlay').data('overlay').close(); } }
+                    });
+            }
+         }
+        });
+
+    /* modified grid table cell/row highlighting cues */
+    jQuery('table.listing td input').click(function(event) {
+        jQuery(this).parent().addClass('modified');
+        jQuery(this).parent().parent().addClass('modified');
+    });
+
+    /* unlock viewers checkbox */
+    jQuery('table.listing td a.unlock').click(function(event) {
+        jQuery(this).parent().children('input.locked').show();
+        jQuery(this).parent().children('a.purge').show().addClass('block');
+        jQuery(this).parent().children('a.password-reset').show().addClass('block');
+        jQuery(this).parent().children('.checkmark').hide();
+        jQuery(this).hide();
+    });
+
+});
