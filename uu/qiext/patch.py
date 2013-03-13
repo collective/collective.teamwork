c03eb779016ac671765ec13647f37a6a94882fac
Sean Upton <sdupton@gmail.com>
2013-03-13 13:17:00 -0600

    monkey patch ATCT ATTopic.buildQuery to respect navigation roots

--- uu/qiext/patch.py
+++ uu/qiext/patch.py
@@ -1,4 +1,6 @@
+from plone.app.layout.navigation.root import getNavigationRoot
 from Products.ATContentTypes.content.schemata import ATContentTypeSchema
+from Products.ATContentTypes.content.topic import ATTopic
 from Products.ATContentTypes.criteria.path import ATPathCriterionSchema
 
 
@@ -9,4 +11,30 @@ def patch_atct_copyrefs():
     """
     ATContentTypeSchema['relatedItems'].keepReferencesOnCopy = True
     ATPathCriterionSchema['value'].keepReferencesOnCopy = True
+
+
+def patch_atct_buildquery():
+    """
+    ATTopic does not filter to navigation root when no location
+    criterion is provided, this is a problem for isolating
+    project workspaces from content of other workspaces.
+    
+    This is monkey-patched instead of fixed upstream because
+    ATTopic is officially deprecated, and plone.app.collection,
+    which may later fully replace it already respects nav roots.
+    """
+    # ref to original unpatched ATTopic.buildQuery() method:
+    orig_buildQuery_method = ATTopic.buildQuery
+
+    # wrapper method injects navroot path when no path:
+    def buildQuery(self):
+        """monkey patch wrapper injects navroot path when path is missing"""
+        q = orig_buildQuery_method(self)
+        if 'path' not in q:
+            navroot_path = getNavigationRoot(self)
+            q['path'] = navroot_path
+        return q
+
+    # apply the monkey patch:
+    ATTopic.buildQuery = buildQuery
 
--- uu/qiext/zope2.py
+++ uu/qiext/zope2.py
@@ -2,7 +2,7 @@ from AccessControl.Permissions import add_user_folders
 from Products.PluggableAuthService import registerMultiPlugin
 
 from uu.qiext.user import localrole
-from uu.qiext.patch import patch_atct_copyrefs
+from uu.qiext.patch import patch_atct_copyrefs, patch_atct_buildquery
 
 registerMultiPlugin(localrole.WorkspaceLocalRoleManager.meta_type)
 
@@ -16,4 +16,5 @@ def initialize(context):
         visibility = None,
         )
     patch_atct_copyrefs()
+    patch_atct_buildquery()
 
