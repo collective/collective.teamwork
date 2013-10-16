6720e3f9135a209c9e4019a7e6d8895b38a8229c
Sean Upton <sdupton@gmail.com>
2013-10-16 11:52:05 -0600

    content type FTIs in profiles, content class implementations for project, workspace

--- collective/teamwork/content/__init__.py
+++ collective/teamwork/content/__init__.py
@@ -1,1 +1,19 @@
-#
+from plone.dexterity.content import Container
+from zope.interface import implements
+
+from interfaces import IWorkspace, IProject
+
+
+class Workspace(Container):
+    """Core workspace class"""
+
+    implements(IWorkspace)
+
+    portal_type = 'collective.teamwork.workspace'
+
+
+class Project(Workspace):
+    """Project workspace is also navigation root"""
+    implements(IProject)
+
+    portal_type = 'collective.teamwork.project'
