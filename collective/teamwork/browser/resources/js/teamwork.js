a85bef53f895364bfc7b6d542c2d931e4ecf7770
Sean Upton <sdupton@gmail.com>
2017-03-08 14:18:34 -0700

    JS rewrite link text to sharing action on toolbar to use text including membership

--- collective/teamwork/browser/resources/js/teamwork.js
+++ collective/teamwork/browser/resources/js/teamwork.js
@@ -3,6 +3,15 @@
 
 (function ($) {
   $(document).ready(function () {
+    var detect = [
+          'body.portaltype-collective-teamwork-project',
+          'body.portaltype-collective-teamwork-team',
+          'body.portaltype-collective-teamwork-workspace',
+        ].join(', ');
+    if ($(detect).length) {
+      // matching body, we are in the context of a workspace...
+      $('#edit-zone #contentview-local_roles a span:last-child').text('Workgroup membership & sharing');
+    }
     console.log('collective.teamwork ready');
   });
 }(jQuery));
