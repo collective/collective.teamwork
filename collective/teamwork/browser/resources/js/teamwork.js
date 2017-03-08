52fe282e074f75efa3f80586bf7ff17e50e361bc
Sean Upton <sdupton@gmail.com>
2017-03-08 13:53:25 -0700

    CSS/JS for bundle, including type icon styles

---
+++ collective/teamwork/browser/resources/js/teamwork.js
@@ -0,0 +1,8 @@
+/*jshint browser: true, nomen: false, eqnull: true, es5:true, trailing:true */
+
+
+(function ($) {
+  $(document).ready(function () {
+    console.log('collective.teamwork ready');
+  });
+}(jQuery));
