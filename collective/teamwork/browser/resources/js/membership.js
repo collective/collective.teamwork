2a581fe5e382747dcf968a55fcc9c6ed149c564e
Sean Upton <sdupton@gmail.com>
2017-04-11 16:59:54 -0600

    minimize cruft from Plone4 membership implementation

--- collective/teamwork/browser/resources/js/membership.js
+++ collective/teamwork/browser/resources/js/membership.js
@@ -2,36 +2,8 @@
 
 (function ($) {
 
-  function hookupOverlays() {
-    console.log('TODO: overlays');
-  }
-
-  function hookupGridModifiedHighlighting() {
-    $('table.listing td input').click(function(event) {
-      $(this).parent().addClass('modified');
-      $(this).parent().parent().addClass('modified');
-    });
-  }
-
-  function hookupUnlockViewers() {
-    $('table.listing td a.unlock').click(function(event) {
-      var unlock = $(this),
-          cell = unlock.parent();
-      // hide the display only checkmark:
-      cell.children('.checkmark').hide();
-      // in its place, show the actual checkbox:
-      cell.children('input.locked').show();
-      // if/as applicable show buttons for purge, password reset, if in DOM:
-      cell.children('a.purge, a.password-reset').show().addClass('block');
-      // Hide the unlock button:
-      unlock.hide();
-    });
-  }
-
   function initMembership() {
-    hookupOverlays();
-    hookupGridModifiedHighlighting();
-    hookupUnlockViewers();
+    console.log('collective.teamwork membership management');  // TODO
   }
 
   $(document).ready(initMembership);
